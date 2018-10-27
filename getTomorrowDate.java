742ee369404266785a9a4b2cb3f1d421bed9bb60
_rika <i@ksqsf.moe>
2018-10-27 09:55:08 -0400

    add getTomorrowDate.java

---
+++ getTomorrowDate.java
@@ -0,0 +1,9 @@
+// Credit:
+// https://www.reddit.com/r/ProgrammerHumor/comments/2dk86l/gettomorrowsdate/
+
+int getTomorrowsDate() {
+    sleep(1000*60*60*24);
+    getCurrentDate();
+}
+
+// Hey, why not save dates in a database? That will be much faster!
